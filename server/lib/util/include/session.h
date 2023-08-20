5a2161e1476283643bfd3c62009af3b2c80a1291
adler <avihai.adler@gmail.com>
2023-08-20 15:55:59 +0300

    the start of the implementation of sessions

--- server/lib/util/include/session.h
+++ server/lib/util/include/session.h
@@ -1,26 +1,60 @@
 #pragma once
 
+#include <time.h>
 #include "ascii_str.h"
 
-/**
- * @brief represent the state of the session
- *
- * the mode state represented by bits.
- * the first (LSB) bit represent the state of the data socket
- * the next bit represent the state of the session itself (logged in or not)
- * i.e.
- * state = LOGGED_IN | DATA_SOCKET_MODE
- */
+enum data_socket_mode {
+  SOCKET_PASSIVE,
+  SOCKET_ACTIVE,
+};
+
 enum session_state {
-  SESSION_PASSIVE,
-  SESSION_ACTIVE = 1,
-  SESSION_LOGGED_IN = 1 << 1,
+  SESSION_LOGIN_REQUIRED, /* PASS reuqired */
+  SESSION_ACTIVE,         /* logged in. accepts commands */
+  SESSION_INVALID,
+};
+
+struct session_sockets {
+  int control_sockfd;
+  int data_sockfd;
+
+  enum data_socket_mode mode;
 };
 
 struct session {
-  int control_sockfd;
-  int data_sockfd;
-  enum session_state mode;
+  time_t last_seen;
+
+  enum session_state state;
+
+  struct session_sockets sockets;
+
   struct ascii_str ip;
   struct ascii_str port;
+
+  struct ascii_str working_dir; /*the root directory. 'user space' is considered to be <working_dir>/<user_name>.
+                                   working_dir better be as absolute path*/
+  struct ascii_str username;
+  struct ascii_str password;
+  struct ascii_str current_dir;
 };
+
+/**
+ * @brief
+ * takes ownership over `ip`, `port`, `username` and `password`
+ *
+ * @param ip
+ * @param port
+ * @param username
+ * @param working_dir
+ * @param state
+ * @param control_sockfd
+ * @return struct session
+ */
+struct session session_create(struct ascii_str *restrict ip,
+                              struct ascii_str *restrict port,
+                              struct ascii_str *restrict username,
+                              struct ascii_str *restrict password,
+                              struct ascii_str *restrict working_dir,
+                              int control_sockfd);
+
+void session_destroy(struct session *session);
---
+++ server/lib/util/src/session.c
@@ -0,0 +1,50 @@
+#include "session.h"
+#include <unistd.h>
+
+// TODO: default data_sockfd shouldn't be -1
+struct session session_create(struct ascii_str *restrict ip,
+                              struct ascii_str *restrict port,
+                              struct ascii_str *restrict username,
+                              struct ascii_str *restrict password,
+                              struct ascii_str *restrict working_dir,
+                              int control_sockfd) {
+
+  if (!working_dir || ascii_str_empty(working_dir)) goto session_create_invalid;
+  if (!username || ascii_str_empty(username)) goto session_create_invalid;
+  if (!password) *password = ascii_str_create(NULL, 0);
+
+  if (!ip || ascii_str_empty(ip)) goto session_create_invalid;
+  if (!port || ascii_str_empty(port)) goto session_create_invalid;
+
+  struct ascii_str wd = ascii_str_create(ascii_str_c_str(working_dir), ascii_str_len(working_dir));
+  if (!ascii_str_contains(working_dir, ascii_str_c_str(&wd))) goto session_create_invalid;
+
+  return (struct session){
+    .ip = *ip,
+    .port = *port,
+    .sockets = {.control_sockfd = control_sockfd, .data_sockfd = -1, .mode = SOCKET_ACTIVE},
+    .state = SESSION_LOGIN_REQUIRED,
+    .username = *username,
+    .password = *password,
+    .working_dir = wd,
+    .current_dir = ascii_str_create(NULL, 0),
+    .last_seen = time(NULL),
+  };
+
+session_create_invalid:
+  return (struct session){.sockets = {.control_sockfd = -1, .data_sockfd = -1, .mode = SOCKET_ACTIVE},
+                          .state = SESSION_INVALID};
+}
+
+void session_destroy(struct session *session) {
+  if (!session) return;
+
+  close(session->sockets.control_sockfd);
+  close(session->sockets.data_sockfd);
+
+  ascii_str_destroy(&session->ip);
+  ascii_str_destroy(&session->port);
+
+  ascii_str_destroy(&session->username);
+  ascii_str_destroy(&session->current_dir);
+}
